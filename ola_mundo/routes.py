b5acd0fd915a0180b703be43067f3066ded82417
Guilherme Portugal <flordavidaho@gmail.com>
2024-11-11 17:37:31 -0300

    projeto

--- ola_mundo/routes.py
+++ ola_mundo/routes.py
@@ -19,9 +19,11 @@ def homepage():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     formlogin = FormLogin()
-    if request.method == 'POST':
-        dados = request.POST.dict()
-        print(dados)
+    if formlogin.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=formlogin.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data):
+            login_user(usuario)
+            return redirect(url_for('perfil', usuario=usuario.username))
     return render_template('login.html', form=formlogin)
 
 
@@ -37,8 +39,7 @@ def criar_conta():
         database.session.add(usuario)
         database.session.commit()
         login_user(usuario, remember=True)
-        return redirect('perfil', usuario=usuario.username)
-
+        return redirect(url_for('perfil', usuario=usuario.username))
     return render_template('criarconta.html', form=formcriarconta)
 
 
@@ -46,6 +47,13 @@ def criar_conta():
 @login_required
 def perfil(usuario):
     return render_template('perfil.html', usuario=usuario)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
 
 
 @app.route('/projetos')
