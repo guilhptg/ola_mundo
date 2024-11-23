bde0ddf3997926060ecbf550fdde549b137b38ca
Guilherme Portugal <flordavidaho@gmail.com>
2024-11-22 23:30:36 -0300

    secrets

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
-import generate_keys
+import secrets
 import os
 
-secrec_key = os.getenv(generate_keys.token_hex(16))
+secrec_key = os.getenv(secrets.token_hex(16))
 
 banco_projetest = "postgresql://banco_projetest_user:rnOtFlEYSJcFvCRqswUGGdsL1hsEtQOh@dpg-ct0aba5umphs73f34s3g-a.oregon-postgres.render.com/banco_projetest"
